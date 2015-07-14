2c0ac99acd662bdc3ba4b8838e250deb22a3e250
Wesley Aptekar-Cassels <W.Aptekar@gmail.com>
2015-07-14 10:04:19 -0700

    Code cleanup

--- main.cpp
+++ main.cpp
@@ -3,24 +3,23 @@
 #include <string>
 #include <algorithm>
 #include <sstream>
-using namespace std;
 
-vector< vector< vector< int > > > primefactors;
-vector <int> combination;
+std::vector< std::vector< std::vector< int > > > primefactors;
+std::vector <int> combination;
 std::vector<int> divisors;
 
 
-vector < int > addObjectToVector(vector <int> startVector, int num){
-	if(std::find(startVector.begin(), startVector.end(), num) == startVector.end()){
+std::vector < int > addObjectToVector(std::vector <int> startVector, int num) {
+	if (std::find(startVector.begin(), startVector.end(), num) == startVector.end()) {
 		startVector.push_back(num);
 	}
 	return startVector;
 }
 
-void go(vector <int> factors,int offset, int k) {
+void go(std::vector <int> factors, int offset, int k) {
 	if (k == 0) {
 		int total = 1;
-		for (int i = 0; i < combination.size(); i++){
+		for (int i = 0; i < combination.size(); i++) {
 			total *= combination[i];
 		}
 		divisors = addObjectToVector(divisors, total);
@@ -28,50 +27,19 @@ void go(vector <int> factors,int offset, int k) {
 	}
 	for (int i = offset; i <= factors.size() - k; ++i) {
 		combination.push_back(factors[i]);
-		go(factors, i+1, k-1);
+		go(factors, i + 1, k - 1);
 		combination.pop_back();
 	}
 }
 
+void writeDivisorsToFile(std::string filename) {
+	std::ofstream divisorFile;
+	divisorFile.open ("divisors.res");
 
-int main(){
-	string line;
-	ifstream infile;
-	infile.open("primefactor.res");
-	while(!infile.eof()) {
-		getline(infile, line);
-		cout << line << endl;
-	
-		std::stringstream test(line);
-		std::string segment;
-		vector < vector < int > > seglist;
-
-		while(std::getline(test, segment, '='))
-		{	
-			vector < int > tempvector;
-			string newSegment = segment;
-			newSegment.erase(std::remove(newSegment.begin(),newSegment.end(),' '),newSegment.end());
-			std::string starsegment;
-			std::stringstream newSegmentStream(newSegment);
-			while(std::getline(newSegmentStream, starsegment, '*')){
-				tempvector.push_back(atoi(starsegment.c_str()));
-			}
-			
-			seglist.push_back(tempvector);
-
-		}
-
-			primefactors.push_back(seglist);
-	
-	}
-
-	ofstream divisorFile;
-  	divisorFile.open ("divisors.res");
-
-	for(int i = 0; i < primefactors.size()-1; i++){
+	for (int i = 0; i < primefactors.size() - 1; i++) {
 		divisors.push_back(1);
 
-		for(int z = 1; z < primefactors[i][0].size(); z++){
+		for (int z = 1; z < primefactors[i][0].size(); z++) {
 			go(primefactors[i][0], 0, z);
 		}
 
@@ -79,7 +47,7 @@ int main(){
 
 		divisorFile << "All the divisors of " << primefactors[i][1][0] << " are ";
 
-		for(int i = 0; i < divisors.size(); i++){
+		for (int i = 0; i < divisors.size(); i++) {
 			if (i != 0) {
 				divisorFile << ", ";
 			}
@@ -90,7 +58,35 @@ int main(){
 	}
 
 	divisorFile.close();
-  	return 0;
+}
+
+
+int main() {
+	std::string line;
+	std::ifstream infile;
+	infile.open("primefactor.res");
+
+	while (!infile.eof()) {
+		getline(infile, line);
+		std::stringstream test(line);
+		std::string segment;
+		std::vector < std::vector < int > > seglist;
+		while (std::getline(test, segment, '='))
+		{
+			std::vector < int > tempvector;
+			std::string newSegment = segment;
+			newSegment.erase(std::remove(newSegment.begin(), newSegment.end(), ' '), newSegment.end());
+			std::string starsegment;
+			std::stringstream newSegmentStream(newSegment);
+			while (std::getline(newSegmentStream, starsegment, '*')) {
+				tempvector.push_back(atoi(starsegment.c_str()));
+			}
+			seglist.push_back(tempvector);
+		}
+		primefactors.push_back(seglist);
+	}
+	writeDivisorsToFile("divisors.res");
+
+	return 0;
 
 }
-	
